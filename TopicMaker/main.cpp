004a2500edbe7b6857086e80ca4e20fe7dd384a3
gdj0208 <gdjsun@naver.com>
2023-02-04 12:06:14 +0900

    Initial Commit

---
+++ TopicMaker/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TopicMaker
+//
+//  Created by 구동준 on 2023/02/04.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
